0e5d03dd9a2458b6d35542fa4791bf0522b163c3
hasanmd91 <hasanmd1991@hotmail.com>
2023-02-21 01:10:10 +0200

    build login

---
+++ src/components/Login.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { auth, logInWithEmailAndPassword } from "../auth/firebase";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loading) return;
+    if (user) navigate("/countries");
+  }, [user, loading, navigate]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <Button onClick={() => logInWithEmailAndPassword(email, password)}>
+        Login
+      </Button>
+      <div>
+        Don't have an account? <Link to="/register">Register</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
